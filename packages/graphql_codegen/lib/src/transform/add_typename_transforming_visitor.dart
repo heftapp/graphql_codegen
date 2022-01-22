411520836c54540e74eacd559b4b7ff02769be0c
Christian Budde Christensen <budde377@gmail.com>
2022-01-22 20:03:33 +0000

    feat: Add excluded rules

--- packages/graphql_codegen/lib/src/transform/add_typename_transforming_visitor.dart
+++ packages/graphql_codegen/lib/src/transform/add_typename_transforming_visitor.dart
@@ -1,32 +1,119 @@
 import 'package:gql/ast.dart';
 import 'package:graphql_codegen/src/transform/transforming_visitor.dart';
 import 'package:graphql_codegen_config/config.dart';
+import 'package:path/path.dart';
 
 const _TYPENAME_FIELD = '__typename';
 
+const _MATCH_WILDCARD = '*';
+const _MATCH_DOUBLE_WILDCARD = '**';
+
 class AddTypenameTransformationVisitor extends RecursiveTransformingVisitor {
-  final GraphQLCodegenConfig config;
-  final List<String> path;
+  final Iterable<Iterable<String>> _excludedPatterns;
 
   AddTypenameTransformationVisitor({
-    required this.config,
-    this.path = const [],
-  });
+    required GraphQLCodegenConfig config,
+  }) : _excludedPatterns =
+            config.addTypenameExcludedPaths.map((e) => e.split(".")).toList();
+
+  AddTypenameTransformationVisitor._internal(this._excludedPatterns);
+
+  @override
+  OperationDefinitionNode visitOperationDefinitionNode(
+    OperationDefinitionNode node,
+  ) {
+    final Set<String> matches = Set.of([node.name?.value].whereType<String>());
+    switch (node.type) {
+      case OperationType.mutation:
+        matches.add('mutation');
+        break;
+      case OperationType.subscription:
+        matches.add('subscription');
+        break;
+      case OperationType.query:
+        matches.add('query');
+    }
+    final visitor = _matchAndSkip(matches);
+
+    return OperationDefinitionNode(
+      name: visitor.visitOne(node.name),
+      directives: visitor.visitAll(node.directives),
+      type: node.type,
+      selectionSet: visitor.visitOne(node.selectionSet),
+      variableDefinitions: visitor.visitAll(node.variableDefinitions),
+    );
+  }
+
+  @override
+  FragmentDefinitionNode visitFragmentDefinitionNode(
+    FragmentDefinitionNode node,
+  ) {
+    final visitor = _matchAndSkip(Set.of([
+      node.name.value,
+      'fragment',
+    ]));
+    return FragmentDefinitionNode(
+      name: visitor.visitOne(node.name),
+      directives: visitor.visitAll(node.directives),
+      selectionSet: visitor.visitOne(node.selectionSet),
+      typeCondition: visitor.visitOne(node.typeCondition),
+    );
+  }
+
+  @override
+  FieldNode visitFieldNode(
+    FieldNode node,
+  ) {
+    final visitor =
+        _matchAndSkip(Set.of([node.alias?.value ?? node.name.value]));
+    return FieldNode(
+      name: visitor.visitOne(node.name),
+      directives: visitor.visitAll(node.directives),
+      alias: visitor.visitOne(node.alias),
+      arguments: visitor.visitAll(node.arguments),
+      selectionSet: visitor.visitOne(node.selectionSet),
+    );
+  }
+
+  AddTypenameTransformationVisitor _matchAndSkip(Set<String> matches) {
+    final processedPatterns = _excludedPatterns
+        .expand(
+          (element) =>
+              element.isNotEmpty && element.first == _MATCH_DOUBLE_WILDCARD
+                  ? [element.skip(1), element]
+                  : [element],
+        )
+        .where(
+          (element) =>
+              element.isNotEmpty &&
+              (element.first == _MATCH_WILDCARD ||
+                  element.first == _MATCH_DOUBLE_WILDCARD ||
+                  matches.contains(element.first)),
+        )
+        .map((element) =>
+            element.isNotEmpty && element.first == _MATCH_DOUBLE_WILDCARD
+                ? element
+                : element.skip(1));
+    return AddTypenameTransformationVisitor._internal(processedPatterns);
+  }
+
+  bool get _isExcluded => _excludedPatterns.any((element) => element.isEmpty);
 
   @override
   SelectionSetNode visitSelectionSetNode(SelectionSetNode node) {
     final hasTypename = node.selections.whereType<FieldNode>().any((element) =>
         element.alias?.value == _TYPENAME_FIELD ||
         (element.alias == null && element.name.value == _TYPENAME_FIELD));
-    if (hasTypename) {
-      return super.visitSelectionSetNode(node);
+
+    var selections = List.of(node.selections);
+
+    if (!hasTypename && !_isExcluded) {
+      final newField = FieldNode(name: NameNode(value: _TYPENAME_FIELD));
+      selections.add(newField);
     }
 
-    final List<SelectionNode> newSelections = List.from(node.selections);
-    final newField = FieldNode(name: NameNode(value: _TYPENAME_FIELD));
-    newSelections.add(newField);
     return SelectionSetNode(
-      selections: super.visitAll(newSelections),
+      selections: visitAll(selections),
     );
   }
 }
