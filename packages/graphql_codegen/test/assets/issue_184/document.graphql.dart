ba68532f26e6836a889cd6c9180999d1db294358
Christian Budde Christensen <budde377@gmail.com>
2022-09-20 19:48:30 +0200

    repro: Issue #184

---
+++ packages/graphql_codegen/test/assets/issue_184/document.graphql.dart
@@ -0,0 +1,149 @@
+import 'package:gql/ast.dart';
+
+class Fragment$F {
+  Fragment$F({
+    this.$default,
+    required this.$__typename,
+  });
+
+  factory Fragment$F.fromJson(Map<String, dynamic> json) {
+    final l$$default = json['default'];
+    final l$$__typename = json['__typename'];
+    return Fragment$F(
+      $default: (l$$default as String?),
+      $__typename: (l$$__typename as String),
+    );
+  }
+
+  final String? $default;
+
+  final String $__typename;
+
+  Map<String, dynamic> toJson() {
+    final _resultData = <String, dynamic>{};
+    final l$$default = $default;
+    _resultData['default'] = l$$default;
+    final l$$__typename = $__typename;
+    _resultData['__typename'] = l$$__typename;
+    return _resultData;
+  }
+
+  @override
+  int get hashCode {
+    final l$$default = $default;
+    final l$$__typename = $__typename;
+    return Object.hashAll([
+      l$$default,
+      l$$__typename,
+    ]);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (!(other is Fragment$F) || runtimeType != other.runtimeType) {
+      return false;
+    }
+    final l$$default = $default;
+    final lOther$$default = other.$default;
+    if (l$$default != lOther$$default) {
+      return false;
+    }
+    final l$$__typename = $__typename;
+    final lOther$$__typename = other.$__typename;
+    if (l$$__typename != lOther$$__typename) {
+      return false;
+    }
+    return true;
+  }
+}
+
+extension UtilityExtension$Fragment$F on Fragment$F {
+  CopyWith$Fragment$F<Fragment$F> get copyWith => CopyWith$Fragment$F(
+        this,
+        (i) => i,
+      );
+}
+
+abstract class CopyWith$Fragment$F<TRes> {
+  factory CopyWith$Fragment$F(
+    Fragment$F instance,
+    TRes Function(Fragment$F) then,
+  ) = _CopyWithImpl$Fragment$F;
+
+  factory CopyWith$Fragment$F.stub(TRes res) = _CopyWithStubImpl$Fragment$F;
+
+  TRes call({
+    String? $default,
+    String? $__typename,
+  });
+}
+
+class _CopyWithImpl$Fragment$F<TRes> implements CopyWith$Fragment$F<TRes> {
+  _CopyWithImpl$Fragment$F(
+    this._instance,
+    this._then,
+  );
+
+  final Fragment$F _instance;
+
+  final TRes Function(Fragment$F) _then;
+
+  static const _undefined = {};
+
+  TRes call({
+    Object? $default = _undefined,
+    Object? $__typename = _undefined,
+  }) =>
+      _then(Fragment$F(
+        $default:
+            $default == _undefined ? _instance.$default : ($default as String?),
+        $__typename: $__typename == _undefined || $__typename == null
+            ? _instance.$__typename
+            : ($__typename as String),
+      ));
+}
+
+class _CopyWithStubImpl$Fragment$F<TRes> implements CopyWith$Fragment$F<TRes> {
+  _CopyWithStubImpl$Fragment$F(this._res);
+
+  TRes _res;
+
+  call({
+    String? $default,
+    String? $__typename,
+  }) =>
+      _res;
+}
+
+const fragmentDefinitionF = FragmentDefinitionNode(
+  name: NameNode(value: 'F'),
+  typeCondition: TypeConditionNode(
+      on: NamedTypeNode(
+    name: NameNode(value: 'Person'),
+    isNonNull: false,
+  )),
+  directives: [],
+  selectionSet: SelectionSetNode(selections: [
+    FieldNode(
+      name: NameNode(value: 'default'),
+      alias: null,
+      arguments: [],
+      directives: [],
+      selectionSet: null,
+    ),
+    FieldNode(
+      name: NameNode(value: '__typename'),
+      alias: null,
+      arguments: [],
+      directives: [],
+      selectionSet: null,
+    ),
+  ]),
+);
+const documentNodeFragmentF = DocumentNode(definitions: [
+  fragmentDefinitionF,
+]);
+const possibleTypesMap = {};
